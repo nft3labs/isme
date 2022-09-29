e71239bf5537ce35ab06649a1a3ac0114919b96c
rockgold0911 <rockgold0911@gmail.com>
2022-09-29 10:40:28 +0800

    feat(app): utils dom add clipboard

---
+++ app/utils/dom/clipboard.ts
@@ -0,0 +1,15 @@
+export const writeText = (text: string) => {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text)
+  } else {
+    const textarea = document.createElement('textarea')
+    document.body.appendChild(textarea)
+    textarea.style.position = 'fixed'
+    textarea.style.clip = 'rect(0 0 0 0)'
+    textarea.style.top = '10px'
+    textarea.value = text
+    textarea.select()
+    document.execCommand('copy', true)
+    document.body.removeChild(textarea)
+  }
+}
