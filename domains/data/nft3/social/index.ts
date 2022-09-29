897e0719851322b3998bbb9f86203a35f491bd72
rockgold0911 <rockgold0911@gmail.com>
2022-09-29 10:40:21 +0800

    fix(domains): social identifier

--- domains/data/nft3/social/index.ts
+++ domains/data/nft3/social/index.ts
@@ -4,9 +4,11 @@ import { useNFT3 } from '@nft3sdk/did-manager'
 import { NFT3Verifier } from '@nft3sdk/client'
 import type { SocialRecord } from './types'
 import { useTwitter } from './hooks'
+import { useNFT3Profile } from 'domains/data'
 
 const useSocialService = () => {
   const { client } = useNFT3()
+  const { did } = useNFT3Profile()
   const [socials, setSocials] = useState<SocialRecord[]>([])
 
   const verifier = useMemo(() => {
@@ -15,7 +17,7 @@ const useSocialService = () => {
 
   const update = useCallback(
     async (identifier?: string) => {
-      identifier = identifier || client.did.identifier
+      identifier = identifier || did
       if (!identifier) return
       const items = await client.socialAccount.list(identifier)
       const rows: SocialRecord[] = items.map((item) => ({
@@ -30,7 +32,7 @@ const useSocialService = () => {
       }))
       setSocials(rows)
     },
-    [client.did.identifier, client.socialAccount, verifier]
+    [client.socialAccount, did, verifier]
   )
 
   const twitter = useTwitter({ socials, verifier })
--- pages/profile-board/[id].tsx
+++ pages/profile-board/[id].tsx
@@ -1,7 +1,6 @@
-import { useMount } from 'app/hooks/useMount'
 import { useNFT3Profile } from 'domains/data'
 import type { GetStaticPaths, GetStaticProps } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProfileBoard from 'UI/profile-board'
 
 export const getStaticProps: GetStaticProps = (props) => {
@@ -22,9 +21,11 @@ export const getStaticPaths: GetStaticPaths = () => {
 
 const IndexPage: FC<{ id: string }> = (props) => {
   const { setDidname } = useNFT3Profile()
-  useMount(() => {
+
+  useEffect(() => {
     setDidname(props.id)
-  })
+  }, [props.id, setDidname])
+
   return <ProfileBoard />
 }
 
