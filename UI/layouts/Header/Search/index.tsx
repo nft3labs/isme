d41a8b1bfe76c9a5589a3e31a80c2a0ba176d5ba
rockgold0911 <rockgold0911@gmail.com>
2022-10-10 08:26:20 +0800

    feat(UI): Header Search add SearchList

--- UI/layouts/Header/Search/index.tsx
+++ UI/layouts/Header/Search/index.tsx
@@ -1,17 +1,111 @@
+import { useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
 import { styled } from '@mui/material/styles'
-import SearchInput from 'components/input-fields/SearchInput'
-import { useState } from 'react'
+import Paper from '@mui/material/Paper'
+import MenuList from '@mui/material/MenuList'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemText from '@mui/material/ListItemText'
+import { TransitionGroup } from 'react-transition-group'
+import Collapse from '@mui/material/Collapse'
+import { useNFT3 } from '@nft3sdk/did-manager'
 
-const ROOT = styled('div')``
+import SearchInput from 'components/input-fields/SearchInput'
+import { useDialog } from 'app/hooks/useDialog'
+import debounce from '@mui/utils/debounce'
+
+interface DIDSearchRecord {
+  didname: string
+  identifier: string
+  addresses: string[]
+}
+
+const ROOT = styled('div')`
+  position: relative;
+`
 
 const Search = () => {
+  const router = useRouter()
+  const { client } = useNFT3()
   const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { visible, open, close } = useDialog({
+    onOpen: () => {
+      setLoading(true)
+    },
+    onClose: () => {
+      setLoading(false)
+    },
+  })
+  const [options, setOptions] = useState<DIDSearchRecord[]>([])
+  const debounceFetcher = useMemo(() => {
+    const loadOptions = (value: string) => {
+      const params = {
+        keyword: value,
+        mode: 'address',
+        limit: 5,
+      }
+      if (!value.startsWith('0x') || value.length !== 42) {
+        params.mode = 'didname'
+      }
+      client.did
+        .search(params as any)
+        .then((data) => {
+          setOptions(data)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+    return debounce(loadOptions, 300)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setValue(() => e.target.value)
+    const { value } = e.target
+    setValue(() => value)
+    if (value) {
+      open()
+      debounceFetcher(value)
+    } else {
+      close()
+    }
   }
   return (
     <ROOT>
-      <SearchInput value={value} onChange={onChange} placeholder="Search by DID name" />
+      <SearchInput
+        value={value}
+        loading={loading}
+        onChange={onChange}
+        placeholder="Search by DID name or wallet address"
+      />
+      <Paper
+        sx={{
+          width: { xs: 1, sm: 450 },
+          position: 'absolute',
+          display: visible ? 'block' : 'none',
+          zIndex: 1,
+        }}
+      >
+        <MenuList>
+          <TransitionGroup>
+            {options.map((option) => (
+              <Collapse key={option.identifier}>
+                <MenuItem
+                  sx={{
+                    padding: '12px 20px',
+                  }}
+                  onClick={() => {
+                    router.push('/profile/' + option.didname)
+                    close()
+                  }}
+                >
+                  <ListItemText>{option.didname}</ListItemText>
+                </MenuItem>
+              </Collapse>
+            ))}
+          </TransitionGroup>
+        </MenuList>
+      </Paper>
     </ROOT>
   )
 }
--- components/input-fields/SearchInput.tsx
+++ components/input-fields/SearchInput.tsx
@@ -1,22 +1,27 @@
+import type { ForwardRefRenderFunction } from 'react'
+import { useRef, forwardRef } from 'react'
 import Paper from '@mui/material/Paper'
 import type { InputBaseProps } from '@mui/material/InputBase'
 import InputBase from '@mui/material/InputBase'
 import IconButton from '@mui/material/IconButton'
 import SearchIcon from '@mui/icons-material/Search'
 import CloseIcon from '@mui/icons-material/Close'
-import { useRef } from 'react'
-import { inputSetValue } from 'app/utils/dom/input'
-import { safeGet } from 'app/utils/get'
-import { useRouter } from 'next/router'
+import CircularProgress from '@mui/material/CircularProgress'
 import { useTheme } from '@mui/material/styles'
 
-const SearchInput: FC<InputBaseProps> = (props) => {
-  const router = useRouter()
+import { inputSetValue } from 'app/utils/dom/input'
+import { safeGet } from 'app/utils/get'
+
+type SearchInputProps = InputBaseProps & {
+  loading: boolean
+}
+const SearchInput: ForwardRefRenderFunction<HTMLFormElement, SearchInputProps> = (props, ref) => {
   const inputRef = useRef<HTMLInputElement>()
   const theme = useTheme()
   return (
     <Paper
       component="form"
+      ref={ref}
       elevation={3}
       sx={{
         p: '2px 4px',
@@ -34,23 +39,18 @@ const SearchInput: FC<InputBaseProps> = (props) => {
         sx={{ p: '10px' }}
         aria-label="search"
         onClick={() => {
-          if (props.value) {
-            router.push('/profile/' + props.value)
-          } else {
-            const input = safeGet(() => inputRef.current)
-            if (input) input.focus()
-          }
+          const input = safeGet(() => inputRef.current)
+          if (input) input.focus()
         }}
       >
-        <SearchIcon />
+        {props.loading ? <CircularProgress size={24} /> : <SearchIcon />}
       </IconButton>
       <InputBase
         inputRef={inputRef}
         sx={{ ml: 1, flex: 1 }}
         {...props}
         onKeyDown={(e) => {
-          if (e.code === 'Enter' && props.value) {
-            router.push('/profile/' + props.value)
+          if (e.code === 'Enter') {
             e.preventDefault()
           }
         }}
@@ -72,4 +72,4 @@ const SearchInput: FC<InputBaseProps> = (props) => {
   )
 }
 
-export default SearchInput
+export default forwardRef(SearchInput)
