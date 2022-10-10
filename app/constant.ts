eb76d3d5603671d1af7e0a146fae34d2daff8388
rockgold0911 <rockgold0911@gmail.com>
2022-10-10 08:26:18 +0800

    feat(constant): change DEFAULT_AVATARS

--- app/constant.ts
+++ app/constant.ts
@@ -4,15 +4,15 @@ type UploadFile = {
   Size: string
 }
 export const DEFAULT_AVATARS: UploadFile[] = [
-  { Name: 'NFT3-0915_1.jpg', Hash: 'QmQogwY3RhGYYLha4MWEF5GcRficmTeSKDv4w6FLiPWP22', Size: '96151' },
-  { Name: 'NFT3-0915_2.jpg', Hash: 'QmaSCKiuFjNc8XLVJHWzgAebjjcthZgwK5SsQEjskBj1oq', Size: '85697' },
-  { Name: 'NFT3-0915_3.jpg', Hash: 'QmZdvaknAamNGbkRmUDU4cNGr3zpBsbsy4zyNbPbt8SEnn', Size: '113590' },
-  { Name: 'NFT3-0915_4.jpg', Hash: 'QmRsEFvyb88vJD4V1WPeUQ7GXRGm3LS34Q3hCSSwZiaJhG', Size: '79762' },
-  { Name: 'NFT3-0915_5.jpg', Hash: 'QmUqYLqPkA4Ch59qBG64yWd2wVbNpRWmxVaXabLE4aaych', Size: '86610' },
-  { Name: 'NFT3-0915_6.jpg', Hash: 'Qme16u3tbeHCt3NGJoSRKFMo4KR5E81AHVcxT2VJbsVvmD', Size: '92041' },
-  { Name: 'NFT3-0915_7.jpg', Hash: 'QmYagbeLLKg4P5GRyZ2zvs2qwkZqBpc4S6iW2PM9ZhkoF2', Size: '86584' },
-  { Name: 'NFT3-0915_8.jpg', Hash: 'QmWLMStxCEG7rF3Xbw4DhKgR5XNTw3PZw4S91Rn99kypAj', Size: '84012' },
-  { Name: 'NFT3-0915_9.jpg', Hash: 'QmVRFtXrsUkWKWyeuovdzAsCTQsTRzpxuMrZWT9haieT9E', Size: '105206' },
+  { Name: 'icon-1001_1.svg', Hash: 'QmS4gPeSaawqrKWhCumNXDvWe2ukVGJBhuk4CTgk5UbszY', Size: '1334' },
+  { Name: 'icon-1001_2.svg', Hash: 'QmNYARzz7YwM4Z1s2AHDpVJ9jyf5bvT18oiHRBviTRetXL', Size: '1476' },
+  { Name: 'icon-1001_3.svg', Hash: 'QmePifEAHCoasrsKjy3NxW7XkhZhiAm3nCViFqZvKHW8N8', Size: '1846' },
+  { Name: 'icon-1001_4.svg', Hash: 'QmXCipJpEsqnuq9yrDLgHNWh7njafk46KRgVn4vRWXY4rQ', Size: '17537' },
+  { Name: 'icon-1001_5.svg', Hash: 'QmQhy9zAkbaPVWgVT8x3qTL8nzDo6UbmC6a8JBZSUVgSSp', Size: '17699' },
+  { Name: 'icon-1001_6.svg', Hash: 'QmPC4bQXpi2x1GAGVU2CJUxnm1X14Cje4hmLUEC2Zkhh8T', Size: '17670' },
+  { Name: 'icon-1001_7.svg', Hash: 'QmYz5j89pgLzcJYzBfqj2x3fSfZbUkVdFCwKytUBzszrBs', Size: '1349' },
+  { Name: 'icon-1001_8.svg', Hash: 'QmPSZth2oHxeYq2QcQXc5UF2tmCotrg3LAuQxC61HEvD45', Size: '1295' },
+  { Name: 'icon-1001_9.svg', Hash: 'QmUJvhbt6sd38etmsU4NNNVFnFPA8SF58QP5DrCxH8xoqR', Size: '1535' },
 ]
 
 export const getFilePath = (file: UploadFile) => {
