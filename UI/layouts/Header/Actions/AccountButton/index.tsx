771c7d19f91efdb8855b340ca2bea2766b77d237
rockgold0911 <rockgold0911@gmail.com>
2023-01-19 08:50:25 +0800

    feat(UI): AccountButton add auto open SelectDialog

--- UI/layouts/Header/Actions/AccountButton/index.tsx
+++ UI/layouts/Header/Actions/AccountButton/index.tsx
@@ -18,6 +18,7 @@ import { useTheme } from '@mui/material/styles'
 import { safeGet } from 'app/utils/get'
 import { useAnchorMenu } from 'app/hooks/useAnchor'
 import GroupAddIcon from '@mui/icons-material/GroupAdd'
+import { useMount } from 'app/hooks/useMount'
 
 const AccountButton: FC = () => {
   const { format } = useNFT3()
@@ -33,6 +34,14 @@ const AccountButton: FC = () => {
     sessionStorage.removeItem('sessionKey')
     logout()
   }
+
+  useMount(() => {
+    setTimeout(() => {
+      if (!account && !didname) {
+        selectDialog.open()
+      }
+    }, 1000)
+  })
 
   if (!account && !didname) {
     return (
